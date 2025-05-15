cc03358537f59268dd198b74f5c4a58135eb0792
iemumer.f <umerfarooq0414@gmail.com>
2025-05-16 04:11:08 +0500

    Added a new file

---
+++ add.py
@@ -0,0 +1,12 @@
+from utils import add
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+print(greet("Umer"))
+print(farewell("Umer"))
+print("2 + 3 =", add(2, 3))
+print("2 - 3 =", subtract(2, 3))
