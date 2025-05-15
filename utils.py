4e8d9494635b7f8875d95dc7ba0284ada79b590a
iemumer.f <umerfarooq0414@gmail.com>
2025-05-16 04:21:49 +0500

    Updated

---
+++ utils.py
@@ -0,0 +1,9 @@
+class Math:
+    @staticmethod
+    def add(a, b): return a + b
+    @staticmethod
+    def subtract(a, b): return a - b
+    @staticmethod
+    def multiply(a, b): return a * b
+    @staticmethod
+    def divide(a, b): return a / b if b != 0 else "Error"
