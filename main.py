d0a04626ec3d960bf6023b0c1af269a7f9319a59
iemumer.f <umerfarooq0414@gmail.com>
2025-05-16 04:17:06 +0500

    Updated main.py file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # main.py
+from utils import add, subtract, multiply, divide
+
+
+
 def greet(name):
     return f"Hello, {name}!"
 
@@ -16,3 +20,13 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print(f"Add: {add(x, y)}")
+print(f"Subtract: {subtract(x, y)}")
+print(f"Multiply: {multiply(x, y)}")
+print(f"Divide: {divide(x, y)}")
